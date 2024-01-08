ca2b2fc9eb941db9da01929a921e43d37d6201ab
Edward Do <edwardtdo@gmail.com>
2024-01-08 14:26:54 +0700

    gitignore

--- src/components/FlashCard.tsx
+++ src/components/FlashCard.tsx
@@ -106,7 +106,7 @@ const styles = StyleSheet.create({
     // shadowOffset: {width: 0, height: 2},
     // shadowOpacity: 0.1,
     // shadowRadius: 8,
-    elevation: 5, // for Android shadow
+    // elevation: 5, // for Android shadow
     padding: 16,
     // margin: 16,
     marginTop: 16,
